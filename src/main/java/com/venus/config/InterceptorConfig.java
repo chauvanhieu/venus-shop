38bb2e377fe9bccd1081bcbe95d48c7028eeac59
chauvanhieu <chauhieu.nina@gmail.com>
2023-08-04 14:32:08 +0700

    done

--- src/main/java/com/venus/config/InterceptorConfig.java
+++ src/main/java/com/venus/config/InterceptorConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.venus.interceptor.AdminAuthInterceptor;
-import com.venus.interceptor.AuthProductInterceptor;
 import com.venus.interceptor.CountElementShoppingCartInterceptor;
 import com.venus.interceptor.LoginInterceptor;
 
@@ -20,8 +19,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 	AdminAuthInterceptor adminAuthInterceptor;
 	@Autowired
 	CountElementShoppingCartInterceptor countShoppingCartInterceptor;
-	@Autowired
-	AuthProductInterceptor authProductInterceptor;
+
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -42,6 +40,5 @@ public class InterceptorConfig implements WebMvcConfigurer {
 		registry.addInterceptor(countShoppingCartInterceptor).addPathPatterns("/**")
 				.excludePathPatterns("/reset-password", "/forgot-password", "/admin/**");
 
-		registry.addInterceptor(authProductInterceptor).addPathPatterns("/api/products/**");
 	}
 }
